cc002926757c3d7990ed1b88011fd964af710645
najongjine <najongjin3@hotmail.com>
2025-11-24 13:14:56 +0900

    .

---
+++ app/(tabs)/MemoEdit.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function MemoEdit() {
+  return (
+    <View>
+      <Text>메모 작성</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
