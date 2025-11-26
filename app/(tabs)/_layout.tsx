404145dff5d37f21f93d12fed5dd1d588984fa95
najongjine <najongjin3@hotmail.com>
2025-11-26 14:16:52 +0900

    .

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,12 +29,23 @@ export default function TabLayout() {
           }}
         />
         <Tabs.Screen
-          name="explore"
+          name="MemoDetail"
           options={{
-            title: "Explore",
+            title: "MemoDetail",
             tabBarIcon: ({ color }) => (
               <IconSymbol size={28} name="paperplane.fill" color={color} />
             ),
+            href: null,
+          }}
+        />
+        <Tabs.Screen
+          name="MemoEdit"
+          options={{
+            title: "MemoEdit",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="paperplane.fill" color={color} />
+            ),
+            href: null,
           }}
         />
       </Tabs>
